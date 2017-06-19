cc286e91064813b0aafb62c4322ace134444e622
Jose Torrez <Jose.Torrez@fundacion-jala.org>
2017-06-19 17:48:29 -0400

    Initial Commit

---
+++ tetris-app/src/main/java/Library.java
@@ -0,0 +1,21 @@
+/**
+ * Created by AbelBarrientos on 6/16/2017.
+ */
+public final class Library {
+
+  /**
+   * Constructor.
+   */
+  private Library() {
+
+  }
+
+  /**
+   * Example method.
+   *
+   * @return Boolean.
+   */
+  public static boolean isTrue() {
+    return true;
+  }
+}
---
+++ tetris-app/src/test/java/LibraryTest.java
@@ -0,0 +1,16 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+/**
+ * Created by AbelBarrientos on 6/16/2017.
+ */
+public class LibraryTest {
+  /**
+   * Example Test.
+   */
+  @Test
+  public void testExample() {
+    assertEquals(true, Library.isTrue());
+  }
+}
